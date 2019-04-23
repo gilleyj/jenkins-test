ed797d75abec610c8e4d2901f43fc75658473add
joel gilley <gilleyj@gmail.com>
2019-04-23 12:40:04 -0400

    Fix

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -4,20 +4,25 @@ SLACK_WEBHOOK=https://hooks.slack.com/services/T04L3RMKF/BJ6AJ40MU/X7XzzsqkWc4zT
 
 echo "notify slack"
 
+json_escape () {
+    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
+}
+
 # post_to_slack channel username icon message hook
 function post_to_slack () {
-	SLACK_CHANNEL=$1
-	SLACK_USER=$2
-	SLACK_ICON=$3
-	SLACK_MESSAGE="\`\`\`$2\`\`\`"
-	SLACK_URL=$4
-
-	curl -X POST --data "payload={\"text\": \"${SLACK_MESSAGE}\", \"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USER}\", \"icon_emoji\": \"${SLACK_ICON}\"}" ${SLACK_URL}
+	channel="\"channel\":$(json_escape "${1}"),"
+	user="\"username\":$(json_escape "${2}"),"
+	icon="\"icon_emoji\":$(json_escape "${3}"),"
+	text="\"text\":$(json_escape "${4}"),"
+	hook=$5
+	
+	payload="payload={$channel$user$icon$text}"
+	echo "$(curl -s -S --data-urlencode "$payload" "$hook" 2>&1)"
 }
 
 chn="#general"
 msg="Jenkins build on ${JOB_NAME} - ${BRANCH_NAME} at ${JENKINS_URL} ${GIT_URL} ${GIT_BRANCH} is failing!"
 icn=":ambulance:"
-usr="gilleyj"
+usr="jenkins"
 
-post_to_slack ${chn} ${usr} ${icn} ${msg} ${SLACK_URL}
+post_to_slack ${chn} ${usr} ${icn} "${msg}" ${SLACK_WEBHOOK}
