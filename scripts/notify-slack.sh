6acf405e7a1b22693e24462c875cf3336699ea94
joel gilley <gilleyj@gmail.com>
2019-04-24 17:52:38 -0400

    Poo

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -12,7 +12,7 @@ function post_to_slack () {
 }
 
 chn="#general"
-msg="*Jenkins build on ${JOB_NAME} is failing!*\n${BUILD_TAG} at ${BUILD_URL} or ${RUN_DISPLAY_URL}\n${GIT_URL} ${GIT_BRANCH}\n"
+msg="*Jenkins build on ${JOB_NAME} is failing! _${STAGE_NAME}_*\n${BUILD_TAG} at ${BUILD_URL} or ${RUN_DISPLAY_URL}\n${GIT_URL} ${GIT_BRANCH}\n"
 icn=":broken_heart:"
 usr="jenkins"
 hook=https://hooks.slack.com/services/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
