0cf20f7487d653609bb0a7561f400cd72c690c85
joel gilley <gilleyj@gmail.com>
2019-04-24 17:33:05 -0400

    Without jq this time

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-SLACK_WEBHOOK=https://hooks.slack.com/services/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
-
-apk add jq
-
-echo "notify slack"
-
-json_escape () {
-    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
-}
-
 # post_to_slack channel username icon message hook
 function post_to_slack () {
-	jq -n \
-		--arg chn "${1}" \
-		--arg usr "${2}" \
-		--arg icn "${3}" \
-		--arg msg "${4}" \
-		'{ channel: $chn, username: $usr, icon_emoji: $icn, text: $msg }' \
-	| \
-	curl -d @- \
+	template='{"channel":"%s","username":"%s","icon_emoji":"%s","text":"%s"}'
+	json_string=$(printf "$template" "${1}" "${2}" "${3}" "${4}")
+	curl -d "${json_string}" \
 		--url "${5}" \
 		-H 'Content-Type: application/json' \
 		-H 'Accept: application/json'
@@ -30,5 +15,6 @@ chn="#general"
 msg="Jenkins build on ${JOB_NAME} - ${BRANCH_NAME} at ${JENKINS_URL} ${GIT_URL} ${GIT_BRANCH} is failing!"
 icn=":ambulance:"
 usr="jenkins"
+hook=https://hooks.slack.com/services/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
 
-post_to_slack ${chn} ${usr} ${icn} "${msg}" ${SLACK_WEBHOOK}
+post_to_slack ${chn} ${usr} ${icn} "${msg}" ${hook}
